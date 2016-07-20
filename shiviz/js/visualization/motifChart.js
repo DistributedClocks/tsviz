309cfdc7a5de1b887836a8b054f04066e6118b78
Harjeet Lalh <Harjeet Lalh>
2016-07-19 23:19:11 -0700

    Vertical scaling implemented.

--- shiviz/js/visualization/motifChart.js
+++ shiviz/js/visualization/motifChart.js
@@ -24,12 +24,13 @@ function MotifChart(motifGroup) {
 	}
 
 	sorted.sort(function(a, b) {
-		//Descending order: largest y to smallest y
+		// Descending order: largest y to smallest y
 		return b.getY() - a.getY();
 	});
 
 	motifPoints = sorted;
 
+	// Fix X co-ordinates to be from 0 to the number of motifs
 	for (i = 0; i < motifPoints.length; i++) {
 		motifPoints[i].setX(i);
 		console.log(motifPoints[i].getY());
@@ -38,15 +39,23 @@ function MotifChart(motifGroup) {
 
 MotifChart.prototype.draw = function() {
 
+	// Chart size
     var width = ((176 > motifPoints.length) ? 176 : motifPoints.length);
     var height = 192;
     var barPadding = 0;
 
+    // Create a scale so that the data bars do not go above the chart height
+   	var yScale = d3.scale.linear()
+   							  .domain([0, d3.max(motifPoints, function(d) { return d.getY(); })])
+   							  .range([0, height]);
+
+   	// Insert an SVG which we will draw our chart
     var $svg = d3.select(".chart")
     			.append("svg")
     			.attr("width", width)
     			.attr("height", height);
 
+    // Insert rectangles for each piece of data
     $svg.selectAll("rect")
     	.data(motifPoints)
     	.enter()
@@ -55,14 +64,12 @@ MotifChart.prototype.draw = function() {
     		return i * (width / motifPoints.length);
     	})
     	.attr("y", function(d) {
-    		return height - d.getY() / 349;
+    		return height - yScale(d.getY());
     	})
     	.attr("width", width / motifPoints.length - barPadding)
     	.attr("height", function(d) {
-    		return d.getY() / 349;
+    		return yScale(d.getY());
     	})
     	.attr("fill", "red");
-
-
 
 };
